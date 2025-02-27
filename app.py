459db1ab33d751619dd62dcdfd63b2d8a95cd7db
Jonas Barletta <jonasbarletta@gmail.com>
2025-02-27 20:35:39 -0300

    feat: criado app streamlit

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print('Hello World!')
+import streamlit as st
+
+def hello_world():
+
+    return 'Hello, Docker!'
+
+def main():
+
+    st.write(hello_world())
+
+
+if __name__ == '__main__':
+
+    main()
